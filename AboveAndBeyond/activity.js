f54ca9c7e219f5e90938c15079d4402e47c50086
Vansh Khanna <74200673+VK7160@users.noreply.github.com>
2022-04-19 14:38:57 +0530

    Update activity.js

--- AboveAndBeyond/activity.js
+++ AboveAndBeyond/activity.js
@@ -1,3 +1,5 @@
+$('#header').load('header.html');
 $('#activity1').load('activity1.html');
 $('#activity2').load('activity2.html');
 $('#activity3').load('activity3.html');
+$('#footer').load('footer.html');
